b95b335371d4a7c60089a7c2492265ce217a6909
aron0029 <aronsellstedt@gmail.com>
2020-03-04 11:16:49 +0100

    mya del sidor

--- light-frame-3/example-minimal/js/NavBar.js
+++ light-frame-3/example-minimal/js/NavBar.js
@@ -3,7 +3,7 @@ class NavBar extends Base {
   render() {
     return /*html*/`
       <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="/">CompuBooks</a>
+        <a class="navbar-brand" href="/">Dhyr & Rumson</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
