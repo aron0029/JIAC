b28375056e5136805cab19cdb82d6985c61dac09
LadyQuinn <kiaa887@hotmail.com>
2020-03-04 11:41:53 +0100

    bytte till svenska

--- light-frame-3/example-minimal/js/StartPage.js
+++ light-frame-3/example-minimal/js/StartPage.js
@@ -5,7 +5,7 @@ class StartPage extends Base {
       <div class="row" route="/" page-title="Start">
         <div class="col-12">
           <h1>Start page</h1>
-          <p>This is the start page.</p>
+          <p>Detta är startsidan.</p>
           <h2> Detta är ny test.</p>
           <p3> nu vil jag pröva om detta hittas <br> och vi vet inte vad man gör och hur vi gör det<p3>
           </div>
