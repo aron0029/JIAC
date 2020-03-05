7f2c4ae5898a502f62d358346d4406164d1fa3d8
aron0029 <aronsellstedt@gmail.com>
2020-03-05 10:49:56 +0100

    bild fix

--- www/js/StartPage.js
+++ www/js/StartPage.js
@@ -5,11 +5,11 @@ class StartPage extends Base {
       <div class="row" route="/" page-title="Start">
         <div class="col-12">
           <h1>Start page</h1>
-          <p>This is the start page.</p>
+          <p><strong>This is the start page.</p>
           <h2> Detta är ny test.</p>
           <h3> vi ska lägga in fina bilder </h3>
           <p3> nu vil jag pröva om detta hittas <br> och vi vet inte vad man gör och hur vi gör det<p3>
-          <img src="/images/favicon.png" alt="..." class="img-thumbnail">
+          <img src="/images/MED0.png" alt="..." class="img-thumbnail">
           </div>
       </div>
     `;
