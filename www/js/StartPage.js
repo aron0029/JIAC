86faf2dca85ab9352fa57fe84ff69701b1e96ff0
FireRay <jonashoglund282@gmail.com>
2020-03-05 21:45:10 +0100

    Added 2 img and overlay text on both img

--- www/js/StartPage.js
+++ www/js/StartPage.js
@@ -11,66 +11,48 @@ class StartPage extends Base {
           </div>
            <img src="/images/idasbild.png" alt="..." class="img-thumbnail">
         </div>
+        
 
 
-        <div class="card-group">
 
-
-  <div class="card">
-
-
-     <div class="card-body">
-
-   <img class="img-fluid" src="/images/balkong-800x682.jpg" alt="Responsive image">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+       
+          <div class="card" style="width:35rem; height: 10rem;">
+          <div class="card-body">
+  <img class="img-fluid" src="/images/vara-bostader.jpg" alt="Responsive image">
+  <div class="card-img-overlay">
+    <p class="card-text">I'm text that has a background image!</p>
   </div>
+</div>
+</div>
 
 
+       <div class="card" style="width:35rem; ">
+       
+          <div class="card-body">
+  <img class="img-fluid" src="/images/boka-vardering.jpg" alt="Responsive image">
+  <div class="card-img-overlay">
+    <p class="card-text">I'm text that has a background image!</p>
   </div>
+</div>
+</div>
 
+         
+      
+  
 
-  <div class="card">
-
-
-  <div class="card-body">
-
-   <img class="img-fluid" src="/images/balkong-800x682.jpg" alt="Responsive image">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-
-
-
-  </div>
-
-
-
-
-        </div>
-
-
-
-
-          <div class="list-group"> 
+      <div class="list-group"> 
             <a href="#" class="list-group-item ">Södermaln</a>
             <a href="#" class="list-group-item">Vasastan</a>
             <a href="#" class="list-group-item ">Östermaln</a>
             
 </div>
-        
-</div>
-      </div>
+
+          
     `;
 
 
-
-
-
-
-
   }
+
+
 
 }
