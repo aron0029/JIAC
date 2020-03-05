22d7f80b855fc1d12647418eb10cbd3afde6a0de
aron0029 <aronsellstedt@gmail.com>
2020-03-05 10:50:58 +0100

    strong

--- www/js/StartPage.js
+++ www/js/StartPage.js
@@ -5,7 +5,7 @@ class StartPage extends Base {
       <div class="row" route="/" page-title="Start">
         <div class="col-12">
           <h1>Start page</h1>
-          <p><strong>This is the start page.</p>
+          <p><strong>This is the start page.</strong></p>
           <h2> Detta är ny test.</p>
           <h3> vi ska lägga in fina bilder </h3>
           <p3> nu vil jag pröva om detta hittas <br> och vi vet inte vad man gör och hur vi gör det<p3>
