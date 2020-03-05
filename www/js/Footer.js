2c8fa08c97d808696f3461c2ff24a6b8853e1472
aron0029 <aronsellstedt@gmail.com>
2020-03-05 14:48:43 +0100

    facebook

--- www/js/Footer.js
+++ www/js/Footer.js
@@ -5,8 +5,12 @@ class Footer extends Base {
       <footer class="bg-primary p-4 container-fluid">
         <div class="row">
           <div class="col text-center text-light">
-            © Dhyr & Rumson
-          </div>
+            © Dhyr & Rumson   
+           
+            <a id="facebook-button" class="btn btn-block btn-social btn-facebook">
+                <i class="fa fa-facebook"></i> följ oss på Facebook
+</a>
+ </div>
         </div>
       </footer>
     `;
