41ef62be99e362317694fe6f3bcb0320dfa217ca
FireRay <jonashoglund282@gmail.com>
2020-03-05 14:26:54 +0100

    testing card

--- www/js/StartPage.js
+++ www/js/StartPage.js
@@ -19,9 +19,25 @@ class StartPage extends Base {
             <a href="#" class="list-group-item ">Södermaln</a>
             <a href="#" class="list-group-item">Vasastan</a>
             <a href="#" class="list-group-item ">Östermaln</a>
+
+
+            <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
         
 </div>
     `;
+
+
+
+
+
+
   }
 
 }
