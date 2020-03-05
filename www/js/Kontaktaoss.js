56b07711c0924cada2fd7bf2048d1eab23f087a7
jaztothemin <jazivarsak@gmail.com>
2020-03-05 10:43:52 +0100

    Changes in h2

--- www/js/Kontaktaoss.js
+++ www/js/Kontaktaoss.js
@@ -6,9 +6,9 @@ class Kontaktaoss extends Base {
         <div class="col-12">
           <h1>kontakta oss</h1>
           <p>T1.</p>
-          <h2> Detta är ny test.</p>
+          <h2> This is DHYR & RUMSON.</p>
           <h3> vi ska lägga in fina bilder </h3>
-          <p3> nu vil jag pröva om detta hittas <br> och vi vet inte vad man gör och hur vi gör det<p3>
+          <p3> Nu vill jag pröva om detta hittas <br> och vi vet inte vad man gör och hur vi gör det<p3>
           </div>
       </div>
     `;
