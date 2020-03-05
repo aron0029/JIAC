520ff0f3c497153b852f708d1eb52ab4e66db47a
aron0029 <aronsellstedt@gmail.com>
2020-03-05 11:18:53 +0100

    fixa bild

--- www/js/StartPage.js
+++ www/js/StartPage.js
@@ -9,7 +9,7 @@ class StartPage extends Base {
           <h2> Detta är ny test.</p>
           <h3> vi ska lägga in fina bilder </h3>
           <p3> nu vil jag pröva om detta hittas <br> Vi borde bete oss som Harvey Specter<p3>
-           <img src="/images/MEDO0.png" alt="..." class="img-thumbnail">
+           <img src="/images/MED0.png" alt="..." class="img-thumbnail">
           </div>
       </div>
     `;
