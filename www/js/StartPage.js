159f280450ca6053d93a9da36bfa6356b8d98222
jaztothemin <jazivarsak@gmail.com>
2020-03-05 13:56:01 +0100

    Pragraphs empty

--- www/js/StartPage.js
+++ www/js/StartPage.js
@@ -4,12 +4,12 @@ class StartPage extends Base {
     return /*html*/`
       <div class="row" route="/" page-title="Start">
         <div class="col-12">
-          <h1>Start page</h1>
+          <h1></h1>
           <center> <p><strong>VÄLKOMMEN TILL MÄKLARFIRMAN DHYR & RUMSON 
            </strong></p> </center>
-          <h2> Detta är ny test.</p>
-          <h3> vi ska lägga in fina bilder </h3>
-          <p3> nu vil jag pröva om detta hittas <br> Vi borde bete oss som Harvey Specter<p3>
+          <h2> </p>
+          <h3>  </h3>
+          <p3>  <br> <p3>
            <img src="/images/MED0.png" alt="..." class="img-thumbnail">
           </div>
       </div>
