ebdba5c6970600125a83cea26def2a487f62de46
FireRay <jonashoglund282@gmail.com>
2020-03-05 15:55:53 +0100

    Added cards to left and to right.

--- www/js/StartPage.js
+++ www/js/StartPage.js
@@ -13,33 +13,43 @@ class StartPage extends Base {
         </div>
 
 
-<div class="card-group">
+        <div class="card-group">
 
-<div class="card">
 
-          <img class="img-fluid" src="/images/balkong-800x682.jpg" alt="Responsive image">
-  <div class="card-body">
+  <div class="card">
+
+
+     <div class="card-body">
+
+   <img class="img-fluid" src="/images/balkong-800x682.jpg" alt="Responsive image">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
-          
-   </div>
 
-   <div class="card">
-    <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
+
+  </div>
+
+
+  <div class="card">
+
+
+  <div class="card-body">
+
+   <img class="img-fluid" src="/images/balkong-800x682.jpg" alt="Responsive image">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+
+
+
   </div>
 
 
 
 
-
-</div>
+        </div>
 
 
 
