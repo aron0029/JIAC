457dd0fea473461c3fbcd957698d3dba9a9ac0f7
jaztothemin <jazivarsak@gmail.com>
2020-03-05 14:30:23 +0100

    changes in image

--- www/js/StartPage.js
+++ www/js/StartPage.js
@@ -4,17 +4,16 @@ class StartPage extends Base {
     return /*html*/`
       <div class="row" route="/" page-title="Start">
         <div class="col-12">
-      
-          <h1></h1>
-          <center> <p><strong>VÄLKOMMEN TILL MÄKLARFIRMAN DHYR & RUMSON 
-           </strong></p> </center>
-          <h2> </p>
-          <h3>  </h3>
-          <p3>  <br> <p3>
-           <img src="/images/4007.jpg" alt="..." class="img-thumbnail">
+          <div class="start-page-text-on-image">
+            <p class="text-center  mb-5"><strong>VÄLKOMMEN TILL MÄKLARFIRMAN DHYR & RUMSON 
+            </strong></p> 
           </div>
+          <img src="/images/idasbild.png" alt="..." class="img-thumbnail">
+        </div>
       </div>
     `;
   }
 
 }
+
+
