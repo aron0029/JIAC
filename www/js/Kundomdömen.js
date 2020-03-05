605ae4e435b5312aef9fe181972f1737145c56c3
my-username-cao-on-github <55875337+Caoabc123@users.noreply.github.com>
2020-03-05 15:19:08 +0100

    Kundomdömen1

---
+++ www/js/Kundomdömen.js
@@ -0,0 +1,13 @@
+class Kundomdomen extends Base {
+
+    render() {
+      return /*html*/`
+        <div class="row" route="/kundomdomen" page-title="Kundomdömen">
+          <div class="col-12">
+            <h1>Lämna gärna ditt omdöme på oss!</h1>
+            <p>Skicka kommentar!</p>
+          </div>
+        </div>
+      `;
+    }
+}
