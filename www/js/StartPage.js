42c1c8e6e2455ca47b692bf3981398961a5caf06
aron0029 <aronsellstedt@gmail.com>
2020-03-06 10:35:23 +0100

    test

--- www/js/StartPage.js
+++ www/js/StartPage.js
@@ -6,7 +6,8 @@ class StartPage extends Base {
       <div class="row" route="/" page-title="Start">
         <div class="col-12">
           <div class="start-page-text-on-image">
-            <p class="text-center  mb-5"><strong>VÄLKOMMEN TILL MÄKLARFIRMAN DHYR & RUMSON 
+            
+          <p class="text-center startsidacolor  mb-5"><strong>VÄLKOMMEN TILL MÄKLARFIRMAN DHYR & RUMSON 
             </strong></p> 
           </div>
            <img src="/images/idasbild.png" alt="..." class="img-thumbnail">
