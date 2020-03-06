1ed4a57307ceed8ddf23b95552d1aaf573212bcf
LadyQuinn <kiaa887@hotmail.com>
2020-03-06 13:11:57 +0100

    tillsalu

--- www/js/App.js
+++ www/js/App.js
@@ -3,14 +3,14 @@ class App extends Base {
   mount() {
     this.navBarLinks = [
       { label: 'Välkommen', route: '/' },
-      { label: 'Till salu', route: '/Till-salu' },
+      { label: 'Till salu', route: '/till-salu' },
       { label: 'Kontakta oss', route: '/kontaktaoss' },
       { label: 'Sälj', route: '/salj' },
     ];
     this.navBar = new NavBar({ links: this.navBarLinks });
     this.footer = new Footer();
     this.startPage = new StartPage();
-    this.aboutUsPage = new Tillsalu();
+    this.tillSalu = new Tillsalu();
     this.missingPage = new MissingPage();
     this.kontaktaoss = new Kontaktaoss();
     this.salj = new Salj();
@@ -24,7 +24,7 @@ class App extends Base {
         </header>
         <main class="container my-4">
           ${this.startPage}
-          ${this.aboutUsPage}
+          ${this.tillSalu}
           ${this.missingPage}
           ${this.kontaktaoss}
           ${this.salj}
