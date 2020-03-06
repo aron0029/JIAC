57020c72a024e266794329aac5ce50231c475c0a
my-username-cao-on-github <55875337+Caoabc123@users.noreply.github.com>
2020-03-06 13:40:20 +0100

    Kundomden5

--- www/js/Kundomdömen.js
+++ www/js/Kundomdömen.js
@@ -15,7 +15,7 @@ class Kundomdomen extends Base {
               <meta charset="UTF-8">
               <meta name="viewport" content="width=device-width, initial-scale=1.0">
               <meta http-equiv="X-UA-Compatible" content="ie=edge">
-              <link rel="stylesheet" href="css/3Style.css">
+              <link rel="stylesheet" href="css/Style.css">
               <title>Formulär</title>
           
           </head>
@@ -34,12 +34,11 @@ class Kundomdomen extends Base {
                   <input type="text" name="epost" id="epost" /> <br /> Meddelande:
           
                   <br />
-                  <textarea name="meddelande" id="meddelande" cols="45" rows="5"></textarea> <br />
-          
+                  <textarea name="meddelande" id="meddelande" cols="45" rows="5"></textarea> <br /> Adress:
                   <br />
-                  <textarea name="land" id="land" cols="60" rows="6"></textarea> <br />
+                  <textarea name="kommun" id="kommun" cols="60" rows="6"></textarea> <br />
           
-                  <select name="land"> 
+                  <select name="kommun"> 
                   <option value="södermalm">Södermalm</option>   
                   <option value="vasastan">Vasastan</option>  
                   <option value="östermalm">Östermalm</option>  
@@ -49,14 +48,15 @@ class Kundomdomen extends Base {
                   <input type="submit " name="skicka " id="skicka " value="Skicka meddelandet " question="vilket nordisk land " />
           
               </form>
+
               <p> Observera att själva formuläret inte syns. </p>
-              
-              <p2>Observera också att standardbredden för ett textinmatningsfält är 30 tecken. </p2>
+
+              <p> Observera också att standardbredden för ett textinmatningsfält är 30 tecken. </p>
           
               <h1>Telefon: 0046-12345678910</h1>
-              <h2>Adress: Äspholmsvägen 16 127 45 Lillholmen, Stockholm, Sverige.</h2>
+              <h2>Adress: Centralplan 15, 111 20 Stockholm, Sverige.</h2>
               <h3>DHYR & RUMSON</h3>
-              <p>Upphovsrätt 2020</p>
+              <h4>Upphovsrätt 2020</h4>
           
           </body>
           
