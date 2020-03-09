8c33a429b0d8c985aeffda2a463025bc56146fda
FireRay <jonashoglund282@gmail.com>
2020-03-09 22:13:55 +0100

    bg-secondary Footer and NavBar plus added some stuff @StartPage

--- www/js/Footer.js
+++ www/js/Footer.js
@@ -2,7 +2,7 @@ class Footer extends Base {
 
   render() {
     return /*html*/`
-      <footer class="bg-primary p-4 container-fluid">
+      <footer class="bg-secondary p-4 container-fluid">
         <div class="row">
           <div class="col text-center text-light">
             © Dhyr & Rumson   
--- www/js/NavBar.js
+++ www/js/NavBar.js
@@ -2,7 +2,7 @@ class NavBar extends Base {
 
   render() {
     return /*html*/`
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
         <a class="navbar-brand" href="/">Dhyr & Rumson</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
