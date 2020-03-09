d02c9deb5038a6ae078df5e83d1cb602b7742db4
FireRay <jonashoglund282@gmail.com>
2020-03-09 20:13:50 +0100

    Startpage with clickable links as images and removed 2 rootlelement from tillslut-file.

--- www/js/StartPage.js
+++ www/js/StartPage.js
@@ -18,7 +18,7 @@ class StartPage extends Base {
 
 <div class="card-group">
  <br></br>
-  <br></br>
+  
 <div class="row">
   <div class="col-sm-6">
     <div class="card">
@@ -40,10 +40,15 @@ class StartPage extends Base {
   <div class="col-sm-6">
     <div class="card">
       <div class="card-body">
-      <img class="img-fluid" src="/images/move.jpg"style="width: 800px; height: 300px">
+
+        <button class="btn btn-default">
+     <img class="img-fluid" src="/images/move.jpg"style="width: 800px; height: 300px">
+     <a href="#"></a>
+      </button>
+      
         <h5 class="display-4">Boka värdering</h5>
         <p class="lead">För en professionell utvärdering från våra mäklare av hem som du vill sälja.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        
       </div>
     </div>
   </div>
@@ -58,14 +63,7 @@ class StartPage extends Base {
   
       </div>
 
-      <div class="list-group" class="row" class="mx-auto" style= "width=400;">
-   
 
-            <a href="#" class="list-group-item ">Södermaln</a>
-            <a href="#" class="list-group-item">Vasastan</a>
-            <a href="#" class="list-group-item ">Östermaln</a>
-            
-</div>
 
 </div>
           
