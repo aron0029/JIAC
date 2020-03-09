7434806ba871238f093862ca4da592f1b0ca3a3f
aron0029 <aronsellstedt@gmail.com>
2020-03-09 14:43:27 +0100

    removed code from co-author to filter

--- www/js/tillsalu.js
+++ www/js/tillsalu.js
@@ -1,32 +1,12 @@
-class tillsalu extends Base {
+class Tillsalu extends Base {
   async mount() {
     sql(/*sql*/`USE databas`);
   }
 
-  async search() {
-    this.result = await sql(/*sql*/`
 
-    SELECT * FROM Residence
-    JOIN Pics
-    ON Residence.residenceId = Pics.residenceId
-    WHERE Kvm >= $minArea
-    AND Kvm <= $maxArea
-    AND price >= $minPrice
-    AND price <= $maxPrice
-  `, {
-      minArea: 20,
-      maxArea: 100,
-      minPrice: 200000,
-      maxPrice: 20000000
 
-    });
-    filterHousesByLocation(houses, keyword) {
-      filteredHouses = houses.filter(a => a.location === keyword);
-      return filteredHouses;
-    }
-
-    render() {
-      return /*html*/`
+  render() {
+    return /*html*/`
         <div class="row" route="/till-salu" page-title="Till salu">
           <div class="col-12">
             <h1>Till salu</h1>
@@ -35,6 +15,6 @@ class tillsalu extends Base {
       </div>
         <img class="img-fluid" src="/images/Södermalm/bild1.1.png" alt="Responsive image"> 
     `;
-    }
-
   }
+
+}
