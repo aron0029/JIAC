c70daa1ea262b3b9b75f8e4f021ff22545772a34
jaztothemin <jazivarsak@gmail.com>
2020-03-09 14:18:46 +0100

    FIX MAP

--- example-minimal/js/StartPage.js
+++ example-minimal/js/StartPage.js
@@ -5,11 +5,8 @@ class StartPage extends Base {
       <div class="row" route="/" page-title="Start">
         <div class="col-12">
           <h1>Start page</h1>
-          <p>Detta är startsidan.</p>
-          <h2> Detta är ny test.</p>
-          <h3> vi ska lägga in fina bilder </h3>
-          <p3> nu vil jag pröva om detta hittas <br> och vi vet inte vad man gör och hur vi gör det<p3>
-          </div>
+          <p>This is the start page.</p>
+        </div>
       </div>
     `;
   }
