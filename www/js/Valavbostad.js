c150805c0ff9a2503937b429d06f2bb818c7e3fb
aron0029 <aronsellstedt@gmail.com>
2020-03-10 16:52:56 +0100

    valavbostadfixed

--- www/js/Valavbostad.js
+++ www/js/Valavbostad.js
@@ -4,11 +4,10 @@ class Valavbostad extends Base {
   render() {
 
     return /*html*/`
-       <div route="/Valavbostad" page-title="valavbostad">
+       <div route="/valavbostad" >
         <h2>${this.streetName} + " " + ${this.streetNumber}</h2>
-        
           <p> detta bostad finns på: ${this.streetName} </p>
-          <p>${this.zip.code}</p>
+          <p>${this.zipcode}</p>
             </div>
   `
   }
