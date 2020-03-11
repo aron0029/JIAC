ca9d7b62a87d0fda38e8750c1f2f9d2060cc5316
LadyQuinn <kiaa887@hotmail.com>
2020-03-11 21:11:15 +0100

    filtering

--- www/js/tillsalu.js
+++ www/js/tillsalu.js
@@ -1,42 +1,61 @@
 class Tillsalu extends Base {
 
-  render() {
+  async mount(){
+    this.anvandarensVal = {
+      kvmMin: 20,
+      kvmMax: 100,
+      minPrice: 200000,
+      maxPrice: 200000000
+    };
+    this.search();
 
+}
+  async search(){
+  this.searchResult = await sql(/*sql*/`
+  SELECT * 
+  FROM Residence
+  /*JOIN Pics
+  ON Residence.residenceId = Pics.PicId*/
+  WHERE kvm >= $kvmMin
+  AND kvm <= $kvmMax
+  AND price >= $minPrice
+  AND price <= $maxPrice
+  `
+   , {
+    kvmMin: 20,
+    kvmMax: 100,
+    minPrice: 200000,
+    maxPrice: 200000000
+  });
+}
+
+  fangaUpp(){
+
+  }
+
+  render() {
+    !this.searchResult ? this.search() : '';
     return /*html*/`
+    ${console.log(this.searchResult)}
        <div route="/till-salu">
-      <h1> Här finner du alla våra bostäder som är till salu </h1>
-        <h2>${this.area} +  ${this.streetName}</h2>
-          <p> detta bostad finns på: ${this.streetNumber} </p>
-          <p>${this.zipCode}</p>
-            </div>
+         <h1> Här finner du alla våra bostäder som är till salu </h1>
+
+         <div class="row">Här ska en form ligga</div>
+
+         <div class="row">
+         ${this.searchResult.map(bostad => /*html*/`
+                    <div class="col-6">
+                    Område: ${bostad.area} Pris: ${bostad.price}kr
+                    </div>
+                  `)}
+         <!--${JSON.stringify(this.searchResult)}-->
+          <!-- <h2>${this.area} +  ${this.streetName}</h2>
+              <p> detta bostad finns på: ${this.streetNumber} </p>
+              <p>${this.zipCode}</p>-->
+          </div>
+        </div>
     ` }
 
 }
 
-    //*  this.searchResult = await sql(/*sql*/`
 
-   //* SELECT * FROM residence
-  //*  --JOIN Pics
-  //*  --ON residence.residenceId = Pics.PicId
-  //*  WHERE kvm >= $kvmMin
-  //*  AND kvm <= $kvmMax
-  //*  AND price >= $minPrice
-  //*  AND price <= $maxPrice
-  //*`
-   //*      , {
-   //*       kvmMin: 20,
-   //*       kvmMax: 100,
-   //*       minPrice: 200000,
-   //*       maxPrice: 200000000
-//*
-   //*     }
-   //*   );
-  //*  }
-
-
-
-
-
-
-
- //* }
