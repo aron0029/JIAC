50b2d3a166b9cd128b303dd34237f90c031826be
aron0029 <aronsellstedt@gmail.com>
2020-03-11 13:25:35 +0100

    Tillsalufix

--- www/js/tillsalu.js
+++ www/js/tillsalu.js
@@ -1,63 +1,42 @@
 class Tillsalu extends Base {
 
-  async mount() {
-    sql(/*sql*/`USE databas`);
+  render() {
+
+    return /*html*/`
+       <div route="/Tillsalu">
+      <h1> Här finner du alla våra bostäder som är till salu </h1>
+        <h2>${this.area} +  ${this.streetName}</h2>
+          <p> detta bostad finns på: ${this.streetNumber} </p>
+          <p>${this.zipCode}</p>
+            </div>
+    ` }
+
+}
+
+    //*  this.searchResult = await sql(/*sql*/`
+
+   //* SELECT * FROM residence
+  //*  --JOIN Pics
+  //*  --ON residence.residenceId = Pics.PicId
+  //*  WHERE kvm >= $kvmMin
+  //*  AND kvm <= $kvmMax
+  //*  AND price >= $minPrice
+  //*  AND price <= $maxPrice
+  //*`
+   //*      , {
+   //*       kvmMin: 20,
+   //*       kvmMax: 100,
+   //*       minPrice: 200000,
+   //*       maxPrice: 200000000
+//*
+   //*     }
+   //*   );
+  //*  }
 
 
 
-    this.searchResult = await sql(/*sql*/`
 
-    SELECT * FROM residence
-    --JOIN Pics
-    --ON residence.residenceId = Pics.PicId
-    WHERE kvm >= $kvmMin
-    AND kvm <= $kvmMax
-    AND price >= $minPrice
-    AND price <= $maxPrice
-  `
-  , {
-      kvmMin: 20,
-      kvmMax: 100,
-      minPrice: 200000,
-      maxPrice: 200000000
 
-  }
-  );
-  }
-  
-    render() {
-      return /*html*/`
-          ${console.log(this.searchResult)}
-          <div class="row" route="/till-salu" page-title="Till salu">
-            <div class="col-12">
-              <h1>Till salu</h1>
-                <p>Här finner du alla våra bostäder som är till salu.</p>
-               <div class="row"> <div class="column"> <img class="img-fluid" 
-                     src="/images/Södermalm/bild1.1.png" style="width:100%">
-                </div>
-                <div class="column">
-                  <img src="/images/södermalm/bild1.5.png" alt="östermalm" style="width:100%">
-                </div>
-                <div class="column">
-                  <img src="/images/södermalm/bild1.4.png" alt="vasastan" style="width:100%">
-                </div>
-              </div>
-  
-     
-  
-              <a href="#" class="list-group-item ">Södermalm</a>
-              <a href="#" class="list-group-item">Vasastan</a>
-              <a href="#" class="list-group-item ">Östermalm</a>
-              
-  </div>
-          </div>
-        
-         
-        </div>
-  
-    
-        
-      `;
-    }
-  
-  }
+
+
+ //* }
