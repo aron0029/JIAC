455bf86e229692aeb999e960b171940fd8da7133
my-username-cao-on-github <55875337+Caoabc123@users.noreply.github.com>
2020-03-12 14:42:19 +0100

    Kundomdömen9

---
+++ example-shop copy/js/Kundomdömen2.js
@@ -0,0 +1,37 @@
+
+
+          
+          
+          <body>
+          <form id="form" name="form" method="post" action=" ">
+      
+              Förnamn:<br />
+              <input type="text" name="fnamn" id="fnamn" /> <br /> Efternamn:
+      
+              <br />
+              <input type="text" name="enamn" id="enamn" /> <br /> E-postadress:
+
+              <br />
+              <input type="text" name="telefon" id="telefon" /> <br /> Telefon:          
+                     
+
+              <br />
+              <input type="text" name="epost" id="meddelande" /> <br /> Meddelande:
+
+              <br />
+              <textarea name="meddelande" id="meddelande" cols="45" rows="5"></textarea> 
+
+
+              <br /> 
+      
+              
+              <button type="button" class="btn btn-primary">Skicka</button>
+          </form>
+         
+      </body>
+                
+                </html>
+
+                </div>
+        
+                <form>
