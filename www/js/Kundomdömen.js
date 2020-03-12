8eb17225075ef8044f956ba7ddc0fd3b3c4bdc81
FireRay <jonashoglund282@gmail.com>
2020-03-12 13:27:21 +0100

    Edited form in Kundomdömen

--- www/js/Kundomdömen.js
+++ www/js/Kundomdömen.js
@@ -8,17 +8,7 @@ class Kundomdomen extends Base {
             <p></p>
           </div>
 
-          <!DOCTYPE html>
-          <html lang="en">
-          
-          <head>
-              <meta charset="UTF-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <meta http-equiv="X-UA-Compatible" content="ie=edge">
-              <link rel="stylesheet" href="css/Style.css">
-              <title>Formulär</title>
-          
-          </head>
+
           
           
           <body>
@@ -36,29 +26,10 @@ class Kundomdomen extends Base {
                   <br />
                   <textarea name="meddelande" id="meddelande" cols="45" rows="5"></textarea> <br/> 
           
-                  <select name="kommun"> 
-                  <option value="södermalm">Södermalm</option>   
-                  <option value="vasastan">Vasastan</option>  
-                  <option value="östermalm">Östermalm</option>  
-                  </select>
-          
-                  <input type="submit " name="skicka " id="skicka "question="vilket nordisk land " />
+                  
                   <button type="button" class="btn btn-primary">Skicka</button>
               </form>
-             
 
-              <p> Observera att själva formuläret inte syns. </p>
-
-              <p> Observera också att standardbredden för ett textinmatningsfält är 30 tecken. </p>
-          
-              <p>Telefon: 0046-12345678910</p>
-              <br> <br/>
-              <p>Adress: Centralplan 15, 111 20 Stockholm, Sverige.</p>
-              
-              <p>DHYR & RUMSON</p>
-              
-              <p>Upphovsrätt 2020</p>
-          
           </body>
           
           
