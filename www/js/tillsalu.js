49185ceaa9187971ba86d4fc7a5153ca76582811
LadyQuinn <kiaa887@hotmail.com>
2020-03-12 16:44:12 +0100

    changes in filtering

--- www/js/tillsalu.js
+++ www/js/tillsalu.js
@@ -3,8 +3,8 @@ class Tillsalu extends Base {
   async mount() {
     this.currencyFormatter = new Intl.NumberFormat('sv-SV', { style: 'currency', currency: 'SEK' });
     this.anvandarensVal = {
-      kvmMin: 10,
-      kvmMax: 300,
+      minKvm: 10,
+      maxKvm: 300,
       minPrice: 100000,
       maxPrice: 100000000,
       chosenDistrict: 'Alla'
@@ -28,8 +28,8 @@ class Tillsalu extends Base {
       FROM Residence
       /*JOIN Pics
       ON Residence.residenceId = Pics.PicId*/
-      WHERE kvm >= $kvmMin
-      AND kvm <= $kvmMax
+      WHERE Kvm >= $minKvm 
+      AND Kvm <= $maxKvm
       AND price >= $minPrice
       AND price <= $maxPrice
       AND (area = $chosenDistrict OR "Alla" = $chosenDistrict)
@@ -58,6 +58,15 @@ class Tillsalu extends Base {
     this.render();
   }
 
+  changeMinKvm(e) {
+    let av = this.anvandarensVal;
+    av.minKvm = e.target.value;
+    if (av.minKvm > av.maxKvm) {
+      av.maxKvm = av.minKvm;
+    }
+    this.render();
+  }
+
   changeMaxPrice(e) {
     let av = this.anvandarensVal;
     av.maxPrice = e.target.value;
@@ -65,6 +74,16 @@ class Tillsalu extends Base {
       av.minPrice = av.maxPrice;
     }
     this.render();
+  }
+
+  changeMaxKvm(e) {
+    let av = this.anvandarensVal;
+    av.maxKvm = e.target.value;
+    if (av.maxKvm < av.minKvm) {
+      av.minKvm = av.maxKvm;
+    }
+    this.render();
+
   }
 
   fixInitialRenderBug() {
@@ -79,47 +98,61 @@ class Tillsalu extends Base {
 
   render() {
     return /*html*/`
-       <div route="/till-salu">
-         <h1 class="mb-3"> Här finner du alla våra bostäder som är till salu </h1>
-         <form>
-           ${this.districtButtons.map(label => /*html*/`
-             <button click="chooseDistrict" type="button" class="btn ${this.anvandarensVal.chosenDistrict === label ?
+    <div route="/till-salu">
+  <h1 class="mb-3"> Här finner du alla våra bostäder som är till salu </h1>
+  <form>
+    ${this.districtButtons.map(label => /*html*/`
+    <button click="chooseDistrict" type="button" class="btn ${this.anvandarensVal.chosenDistrict === label ?
         'btn-primary' : 'btn-secondary'} btn-lg">${label}</button>
-           `)}
-          <div class="form-group mt-3">
-             <input type="text" placeholder="Sök" class="form-control">
-           </div>
-         </form>
-            
-            <form>
-            <div class="search-input__label push-half--left">KVM</div>
-            <div>
-            <div class="form-group">
-            <input type="range" class="form-control-range" id="formControlRange" value="0">
-            <div class="search-input__label push-left">
-            LÄGSTA PRIS ${this.currencyFormatter.format(this.anvandarensVal.minPrice)}</div >
-      <input type="range" class="form-control-range" id="formControlRange" input="changeMinPrice" value="${this.anvandarensVal.minPrice}" min="100000" max="100000000" step="50000">
-<br><br>
-        HÖGSTA PRIS ${this.currencyFormatter.format(this.anvandarensVal.maxPrice)}</div >
-      <input type="range" class="form-control-range" id="formControlRange" input="changeMaxPrice" value="${this.anvandarensVal.maxPrice}" min="100000" max="100000000" step="50000">
-            </div>
-            </form >
-
+    `)}
+    <div class="form-group mt-3">
+      <input type="text" placeholder="Sök" class="form-control">
+    </div>
 
       <div class="row">
-        ${this.searchResult && this.searchResult.map(bostad => /*html*/`
-                    <div class="card" style="width: 50rem;"> <div class="card" style="hight: 220rem;">
-                     ${bostad.area} Pris: ${bostad.price}kr <br> ${bostad.rooms} Rum
-                     men kök lägnehet på ${bostad.Kvm} Kvm  <br>Avgift på ${bostad.rent} kr<br>${bostad.residenceId}
-                     <br>
-                    </div>  
-                  `)}
-        <!{JSON.stringify(this.searchResult)}-->
-          <!-- <h2>${this.area} +  ${this.streetName}</h2>
+        <div class="col-6">Jag är till vänster</div>
+        <div class="col-6">Jag är till höger</div>
+      </div>
+
+
+    <div class="search-input__label push-left">
+      LÄGSTA PRIS ${this.currencyFormatter.format(this.anvandarensVal.minPrice)}
+    </div>
+    <input type="range" class="form-control-range" id="formControlRange" input="changeMinPrice"
+      value="${this.anvandarensVal.minPrice}" min="100000" max="100000000" step="50000">
+    <br><br>
+
+      LÄGSTA KVM ${this.currencyFormatter.format(this.anvandarensVal.minKvm)}
+<input type="range" class="form-control-range" id="formControlRange" input="changeMinKvm"
+  value="${this.anvandarensVal.minKvm}" min="100000" max="100000000" step="50000">
+<br><br>
+
+HÖGSTA PRIS ${this.currencyFormatter.format(this.anvandarensVal.maxPrice)}
+<input type="range" class="form-control-range" id="formControlRange" input="changeMaxPrice"
+  value="${this.anvandarensVal.maxPrice}" min="100000" max="100000000" step="50000">
+
+HÖGSTA KVM ${this.currencyFormatter.format(this.anvandarensVal.minKvm)}
+<input type="range" class="form-control-range" id="formControlRange" input="changeMaxKvm"
+  value="${this.anvandarensVal.maxKvm}" min="100000" max="100000000" step="50000">
+</form>
+
+
+
+<div class="row">
+  ${this.searchResult && this.searchResult.map(bostad => /*html*/`
+  <div class="card" style="width: 50rem;">
+    <div class="card" style="hight: 220rem;">
+      ${bostad.area} Pris: ${bostad.price}kr <br> ${bostad.rooms} Rum
+      men kök lägnehet på ${bostad.Kvm} Kvm <br>Avgift på ${bostad.rent} kr<br>${bostad.residenceId}
+      <br>
+    </div>
+    `)}
+    <!{JSON.stringify(this.searchResult)}-->
+      <!-- <h2>${this.area} +  ${this.streetName}</h2>
         <p> detta bostad finns på: ${this.streetNumber} </p>
         <p>${this.zipCode}</p>-->
-          </div>
-        </div >
+  </div>
+</div>
       ` }
 
 }
