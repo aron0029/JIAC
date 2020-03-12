92cb53288204d3fb6bede8493088edad08170871
LadyQuinn <kiaa887@hotmail.com>
2020-03-12 14:40:46 +0100

    acceptchange

--- www/js/Kundomdömen.js
+++ www/js/Kundomdömen.js
@@ -8,17 +8,7 @@ class Kundomdomen extends Base {
             <p></p>
           </div>
 
-          <!DOCTYPE html>
-          <html lang="en">
-          
-          <head>
-              <meta charset="UTF-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <meta http-equiv="X-UA-Compatible" content="ie=edge">
-              <link rel="stylesheet" href="css/Style.css">
-              <title>Formulär</title>
-          
-          </head>
+
           
           
           <body>
@@ -29,36 +19,24 @@ class Kundomdomen extends Base {
           
                   <br />
                   <input type="text" name="enamn" id="enamn" /> <br /> E-postadress:
-          
+
                   <br />
-                  <input type="text" name="epost" id="epost" /> <br /> Meddelande:
-          
+                  <input type="text" name="telefon" id="telefon" /> <br /> Telefon:          
+                         
+
                   <br />
-                  <textarea name="meddelande" id="meddelande" cols="45" rows="5"></textarea> <br /> 
+                  <input type="text" name="epost" id="meddelande" /> <br /> Meddelande:
+
+                  <br />
+                  <textarea name="meddelande" id="meddelande" cols="45" rows="5"></textarea> 
+
+
+                  <br /> 
           
-                  <select name="kommun"> 
-                  <option value="södermalm">Södermalm</option>   
-                  <option value="vasastan">Vasastan</option>  
-                  <option value="östermalm">Östermalm</option>  
-                  </select>
-          
-                  <input type="submit " name="skicka " id="skicka "question="vilket nordisk land " />
+                  
                   <button type="button" class="btn btn-primary">Skicka</button>
               </form>
              
-
-              <p> Observera att själva formuläret inte syns. </p>
-
-              <p> Observera också att standardbredden för ett textinmatningsfält är 30 tecken. </p>
-          
-              <p>Telefon: 0046-12345678910</p>
-              <br> <br/>
-              <p>Adress: Centralplan 15, 111 20 Stockholm, Sverige.</p>
-              
-              <p>DHYR & RUMSON</p>
-              
-              <p>Upphovsrätt 2020</p>
-          
           </body>
           
           
