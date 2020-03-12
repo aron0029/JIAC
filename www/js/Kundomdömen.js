2f043e782164b43bb3842302776b94da3830f773
aron0029 <aronsellstedt@gmail.com>
2020-03-12 22:39:15 +0100

    kund

--- www/js/Kundomdömen.js
+++ www/js/Kundomdömen.js
@@ -18,11 +18,7 @@ class Kundomdomen extends Base {
     console.log(formData);
     // Send the form data to the db
     await sql(/*sql*/`
-<<<<<<< HEAD
-      INSERT INTO Customer (firstName, lastName, phoneNr) VALUES($firstName, $lastName, $phoneNr)
-=======
       INSERT INTO Customer (firstName, lastName, phoneNr, email, message) VALUES($firstName, $lastName, $phoneNr, $email, $message)
->>>>>>> 24958e7e8f481c87d9438bb0e4a29898524af4ea
     `, formData);
     // Update the "flag" formSent
     this.formSent = true;
@@ -41,27 +37,6 @@ class Kundomdomen extends Base {
 
           ${this.formSent ? /*html*/`
             <div class="col-12">
-<<<<<<< HEAD
-              <h1>Tack för din återkoppling</h1>
-            </div>
-           ` :/*html*/`
-           <div class="col-12">
-            
-            <form submit="collectFormData">
-              <div class="form-group">
-                <label class="w-100">Förnamn
-                  <input name="firstName" type="text" class="form-control" placeholder="Ditt förnamn" required pattern=".{2,}">
-                </label>
-              </div>
-               <div class="form-group">
-                <label class="w-100">Efternamn
-                  <input name="lastName" type="text" class="form-control" placeholder="Ditt efternamn" required>
-                </label>
-              </div>
-              <div class="form-group">
-                <label class="w-100">Telefon
-                  <input name="phoneNr" type="text" class="form-control" placeholder="Telefon" required>
-=======
               <h1 class="display-3">Tack för din återkoppling</h1>
             </div>
            ` :/*html*/`
@@ -92,48 +67,34 @@ class Kundomdomen extends Base {
               <div class="form-group">
                 <label class="w-100">Telefon
                   <input name="phoneNr" type="text" class="form-control" placeholder="+46(0)76010101" required>
->>>>>>> 24958e7e8f481c87d9438bb0e4a29898524af4ea
                 </label>
               </div>
 
 
-<<<<<<< HEAD
-              <input class="btn btn-primary float-right" type="submit" value="Skicka info">
-            </form>
-          </div>
-`}
-
-
-
-
-
-  </div>
-=======
 
                 <div class="form-group">
                 <label class="w-100">E-postadress
                   <input name="email" type="email" class="form-control" placeholder="name@example.com" required>
-                
+
                   </label>
                   </div>
 
-          
+
             <div class="form-group">
                 <label class="w-100">Meddelande
                   <textarea name="message" class="form-control" placeholder="Lämna dina synpunkter" required></textarea>
                 </label>
               </div>
-             
+
 
               <input class="btn btn-secondary float-left" type="submit" value="Skicka">
             </form>
           </div>
            </div>
-          
+
 `}
 
 
->>>>>>> 24958e7e8f481c87d9438bb0e4a29898524af4ea
-      `;
+  `;
   }
 }
