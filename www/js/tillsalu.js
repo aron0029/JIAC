eecbaafe9e11ab751ac2e6b76bdc7842c6fbc84c
aron0029 <aronsellstedt@gmail.com>
2020-03-12 09:13:06 +0100

    hotfix

--- www/js/tillsalu.js
+++ www/js/tillsalu.js
@@ -1,7 +1,13 @@
 class Tillsalu extends Base {
 
 
+  async mount() {
 
+    this.tilSalu = await (Tillsalu, /*sql*/`
+      SELECT * FROM Residence
+    `);
+
+  }
 
 
 
@@ -15,38 +21,7 @@ class Tillsalu extends Base {
     return /*html*/`
 <div route="/till-salu">
 
-<div class="carousel-item">  
 
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="Södermalm" src="/images/Södermalm/bild5.2.png" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="östermalm" src="/images/Södermalm/bild3.2.png" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="/images/Södermalm/bild4.3.png" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-<img src="/images/Södermalm/bild3.3.png" alt="...">
-  <div class="carousel-caption d-none d-md-block">  
-  </div>
-</div>
         <h2> Denna lägenhet finns på </h2>
         <h1> ${this.streetName}${this.streetNumber} </h1>
         <h2> </h2>
