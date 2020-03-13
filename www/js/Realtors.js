b678deabddb7ba359a0d2e2473ac6381a2e91b79
FireRay <jonashoglund282@gmail.com>
2020-03-13 11:19:57 +0100

    Added pics to Realtors class.

--- www/js/Realtors.js
+++ www/js/Realtors.js
@@ -14,13 +14,14 @@ class Realtors extends Base {
       <div route="/varamaklare">
 <div class="row">
 ${this.realtors.map(realtor => /*html*/`
-  	<div class="card" style="width: 20rem;">
-
+    <div class="card" style="width: 18rem;">
+    <img src ="${realtor.pic}" style="width: 18rem;">
   		<div class="card-body">
     		<h5 class="card-title">${realtor.firstName} ${realtor.lastName}</h5>
     		<h6 class="card-subtitle mb-2 text-muted">Mäklare</h6>
     		<p class="card-text">Kontakta mig nedan.</p>
-    		<a href="#" class="card-link">${realtor.email}</a>
+        <a href="#" class="card-link">${realtor.email}</a>
+        <br>
     		<a href="#" class="card-link">${realtor.phoneNr}</a>
 
 
