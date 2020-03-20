14e7eb05556c8620ac040cbbbb90c47e4470312b
FireRay <jonashoglund282@gmail.com>
2020-03-20 15:11:19 +0100

    Added new table and Salj form is now fully function.

--- www/js/Salj.js
+++ www/js/Salj.js
@@ -2,18 +2,26 @@
 class Salj extends Base {
 
   async collectFormData(e) {
-    e.preventDeFault();
+    // Prevent the browser from reloading
+    // the page when we submit the form
+    e.preventDefault();
+    // The form is the event target
     let form = e.target;
+    // Create an object to collect form data in
     let formData = {};
-    for (let element of form.element) {
+    // Loop through the elements of the form
+    for (let element of form.elements) {
       if (!element.name) { continue; }
       formData[element.name] = element.value;
     }
     console.log(formData);
+    // Send the form data to the db
     await sql(/*sql*/`
-      INSERT INTO saljFormular (email, phoneNr, livingArea, message) VALUES($email,$phone, $livingArea,
-      $message) `, formData);
+      INSERT INTO saljFormular (firstName, lastName, email, phoneNr, message, livingArea) VALUES($firstName, $lastName, $email, $phoneNr, $message, $livingArea)
+    `, formData);
+    // Update the "flag" formSent
     this.formSent = true;
+    // Now render again so that the screen updates
     this.render();
   }
 
@@ -26,6 +34,10 @@ class Salj extends Base {
           </h1>
           <p>På Dhyr och Rumson vet vi vad som krävs för att göra en riktigt bra bostadsaffär. Förutom att vi har skickliga mäklare, mycket god lokalkännedom och vårt köparregister Boagenten, så prioriterar vi alltid kundens önskemål. Oavsett om du är redo att sälja nu eller i framtiden så kommer vi gärna hem till dig för att göra en värdering av din nuvarande bostad. Det är helt kostnadsfritt och du förbinder dig inte till något.
             <div class="myline"></div>
+
+
+
+
             <h2 class="headline">Jag önskar att boka en värdering!</h2>
 
             <p>Vill du veta mer om hur vi kan hjälpa dig med din bostadsaffär? Fyll i formuläret, så kontaktar vi dig.</p>
@@ -34,49 +46,60 @@ class Salj extends Base {
             Boka en värdering!</p>
   
 
-        ${this.formSent ? /*html*/`
+${this.formSent ? /*html*/`
             <div class="col-12">
               <h1 class="display-3">Tack för din återkoppling</h1>
             </div>
-           `
-        :
-       /*html*/`
-     
-          <form>
-            <div class="form-group">
-              <label for="exampleFormControlInput1">Email address</label>
-              <input name="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-            </div>
-            <div class="form-group">
-              <label for="exampleFormControlInput1">Telefonnummer</label>
-              <input name="phone" class="form-control" id="exampleFormControlInput1" placeholder="+46073000000">
+           ` :/*html*/`
+
+           
+            <form submit="collectFormData">
+
+              <div class="form-group">
+
+
+              <div class="form-group">
+              <label for="exampleFormControlInput1">Namn</label>
+              <input name="firstName" class="form-control" placeholder="Namn">
               </div>
-            <div class="form-group">
+
+              <div class="form-group">
+              <label for="exampleFormControlInput1">EfterNamn</label>
+              <input name="lastName" class="form-control" placeholder="Efternamn">
+              </div>
+
+              <label for="exampleFormControlInput1">Email address</label>
+              <input name="email" class="form-control" placeholder="name@example.com">
+            </div>
+
+              <div class="form-group">
+              <label for="exampleFormControlInput1">Telefonnummer</label>
+              <input name="phoneNr" class="form-control" placeholder="+46073000000">
+              </div>
+          
+
+              <div class="form-group">
               <label for="exampleFormControlSelect1">Vart bor du?</label>
-              <select name="livingArea" class="form-control" id="exampleFormControlSelect1">
+              <select name="livingArea" class="form-control">
                 <option>Östermalm</option>
                 <option>Södermalm</option>
                 <option>Vasastan</option>
               </select>
             </div>
-            <div class="form-group">
-              <label for="exampleFormControlSelect2">Vad vill du sälja?</label>
-              <select class="form-control" id="exampleFormControlSelect2">
-                <option>Min Lägenhet</option>
 
-              </select>
-            </div>
-            <div class="form-group">
+               <div class="form-group">
               <label for="exampleFormControlTextarea1">Skriv en förklaring av din bostad.</label>
-              <textarea name="message" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <textarea name="message" class="form-control" rows="3"></textarea>
               <br>
-              <button>Skicka</button>
-            </div >
-          </form >
-        </div >
-      </div >
-        
-        `}
+            
+
+
+              <input class="btn btn-secondary float-left" type="submit" value="Skicka">
+            </form>
+          </div>
+           </div>
+          
+`}
 
 
       `;
