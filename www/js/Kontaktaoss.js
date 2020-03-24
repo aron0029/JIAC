a4754af0e064f299ac91e0ce6d07ea6b8a9c3c73
FireRay <jonashoglund282@gmail.com>
2020-03-24 18:22:46 +0100

    Added map

--- www/js/Kontaktaoss.js
+++ www/js/Kontaktaoss.js
@@ -59,7 +59,7 @@ Org.nr: 556444-4593
         <br>
           
         </h6>
-          
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5018.099597491717!2d18.085618635540456!3d59.33227023793569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d5108d06f43%3A0x1919bf925b4f7e2f!2sStrandv%C3%A4gen%2C%20Stockholm!5e0!3m2!1ssv!2sse!4v1585068750631!5m2!1ssv!2sse" width="500" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
 </div>
 
