11979669097c1278ddc35f02aa82290a84da719e
jaztothemin <jazivarsak@gmail.com>
2020-03-26 21:15:01 +0100

    ManyChangesInDesign

--- www/js/NavBar.js
+++ www/js/NavBar.js
@@ -2,10 +2,10 @@ class NavBar extends Base {
 
   render() {
     return /*html*/`
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <img class="img-fluid" src="/images/drlogo13.png" style="width:123px; height:90px">
-        <a class="navbar-brand" href="/"> </a>
-        <img src ="" style="width: 19rem;">
+        <a class="navbar-brand" href="/"> DHYR & RUMSON</a>
+        <img src ="" style="width: 10rem;">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
