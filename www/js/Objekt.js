608a2c7e7825d7541c4c515bbf13802e0142387f
FireRay <jonashoglund282@gmail.com>
2020-03-26 21:56:25 +0100

    Changes made in objekt view

--- www/js/Objekt.js
+++ www/js/Objekt.js
@@ -6,36 +6,74 @@ class Objekt extends Base {
     WHERE residenceId = $id
   `, { id: params.id });
     Object.assign(this, info[0]);
-
-
     ;
-    // FÖRST NÄR DEN HÄR CONSOLE.loggen säger 1 rad
-    // har vi lyckats skriva vår databas fråga... (join)
-    // Kanske börja jobba i databasen först?
-    // Kanske skriv en färdig vy och spara i databasen
-    // För då kan vi ställa frågan mot vyn här i vår JS.
-    // Och slipper komplicerad kod i SQL här i denna fil
-    // Istället så sparar vi vår JOIN som en vy i databasen.
-    console.log("HUR MÅNGA RADER FRÅN DATABASEN?", info.length);
-    Object.assign(this, info[0] || {});
-    console.log("HEPP", this)
-    this.render();
 
   }
 
   render() {
     return /*html*/`
         <div class="row" route="/objekt/:id" page-title="Objekt">
-        <div class="col-12">
-         
+       
           
-                <img class ="card-img" src="${this.pics}">
+       <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
+        	<br />
+        	
+        	<ul class="nav nav-tabs nav-justified">
+        		<li class="nav-item">
+        			<a href="#home" class="nav-link active" role="tab" data-toggle="tab">Info</a>
+        		</li>
+
+        		<li class="nav-item">
+        			<a href="#profile" class="nav-link" role="tab" data-toggle="tab">Bilder</a>
+        		</li>
+
+        		<li class="nav-item">
+        			<a href="#about" class="nav-link" role="tab" data-toggle="tab">Kontakt</a>
+        		</li>
+        	</ul>
+
+          <div class="tab-content">
+          
+            <div role="tabpanel" class="tab-pane fade in active" id="home">
+
+            <br>
+              <p><strong> &nbsp Pris: </strong>${this.price}</p>
+              <p><strong> &nbsp Område: </strong>${this.area}</p>
+              <p><strong> &nbsp Kvm: </strong>${this.Kvm}</p>
+              <p><strong> &nbsp Rum: </strong>${this.rooms}</p>
+              <p><strong> &nbsp Avgift: </strong>${this.rent}</p>
+              <p><strong> &nbsp Adress: </strong>${this.streetName} ${this.streetNumber}</p>
+              <p><strong> &nbsp Församling: </strong>${this.community}</p>
+              <p><strong> &nbsp Förening: </strong>${this.association}</p>
               
+            
+            </div>
+
+            
+            <div role="tabpanel" class="tab-pane fade" id="profile">
+            
+            <img src ="${this.pic}" style="width: 18rem;" >
+            
+            
+            </div>
+
+
+
+            <div role="tabpanel" class="tab-pane fade" id="about">
+            <br>
+            <p><strong> &nbsp Ansvarig mäklare: </strong>${this.firstName} ${this.lastName} </p>
+            <p><strong> &nbsp Mejl: </strong>${this.email} </p>
+            <p><strong> &nbsp Telefon: </strong>${this.phoneNr} </p>
+            <img src ="/${this.realtorPic}" style="width: 18rem;" >
+            
+            
+            </div>
+        	</div>
+        </div>
 
 
         
-          </div >
-          
+      
             </div >
       `
 
