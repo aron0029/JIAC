53c3153c43800591deb8605a51e438bd9efb4798
FireRay <jonashoglund282@gmail.com>
2020-03-27 12:37:33 +0100

    Changes in tillsalut

--- www/js/tillsalu.js
+++ www/js/tillsalu.js
@@ -124,7 +124,7 @@ class Tillsalu extends Base {
     <div route="/kopa-bostad">
     <br>
     <br>
-    <h1 class="mb-3"> Köpa bostad i Södermalm, Östermalm eller Vasastan?</h1><br>
+    <h1 class="mb-3"> Köpa bostad på Södermalm, på Östermalm eller i Vasastan?</h1><br>
     <h2>Här hittar du vårt utbud av bostäder och information om hur du bäst går tillväga för att köpa din nya bostad.</h2>
     <form>
     <br>
