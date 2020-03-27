62230c08f22f12137ab6b6f736e3b45145cea320
FireRay <jonashoglund282@gmail.com>
2020-03-27 11:15:21 +0100

    Changes to objekt page

--- www/js/Objekt.js
+++ www/js/Objekt.js
@@ -32,53 +32,76 @@ class Objekt extends Base {
 
 
 
-       <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         	<br />
-
-        	<ul class="nav nav-tabs nav-justified">
+        	
+        	<ul class="nav nav-tabs">
         		<li class="nav-item">
-        			<a href="#home" class="nav-link active" role="tab" data-toggle="tab">Info</a>
+        			<a href="#info" class="nav-link active" role="tab" data-toggle="tab">Info</a>
         		</li>
-        		<li class="nav-item">
-        			<a href="#profile" class="nav-link" role="tab" data-toggle="tab">Bilder</a>
-        		</li>
-        		<li class="nav-item">
-        			<a href="#about" class="nav-link" role="tab" data-toggle="tab">Kontakt</a>
-        		</li>
-        	</ul>
-          <div class="tab-content">
 
+        		<li class="nav-item">
+        			<a href="#bilder" class="nav-link" role="tab" data-toggle="tab">Bilder</a>
+        		</li>
 
-            <div role="tabpanel" class="tab-pane fade in active" id="home">
-            <br>
-              <p><strong> &nbsp Pris: </strong>${this.price}</p>
+        		<li class="nav-item">
+        			<a href="#kontakt" class="nav-link" role="tab" data-toggle="tab">Kontakt</a>
+        		</li>
+
+        		
+
+        	<div class="tab-content">
+            <div role="tabpanel" class="tab-pane fade in active" id="info">
+            
+               <p><strong> &nbsp Pris: </strong>${this.price}</p>
               <p><strong> &nbsp Område: </strong>${this.area}</p>
               <p><strong> &nbsp Kvm: </strong>${this.Kvm}</p>
               <p><strong> &nbsp Rum: </strong>${this.rooms}</p>
               <p><strong> &nbsp Avgift: </strong>${this.rent}</p>
               <p><strong> &nbsp Adress: </strong>${this.streetName} ${this.streetNumber}</p>
               <p><strong> &nbsp Församling: </strong>${this.community}</p>
-              <p><strong> &nbsp Förening: </strong>${this.association}</p>
-              
+              <p><strong> &nbsp Förening: </strong> ${this.association}</p>
+            
+            
+            
+            
             
             </div>
             
-            <div role="tabpanel" class="tab-pane fade" id="profile">
-            <img  class="card-img" src= "${this.pics}">
+
+
+
+
+            <div role="tabpanel" class="tab-pane fade" id="bilder">
+            
+            <br>
             <img src ="${this.pic}" style="width: 18rem;" >
             
             
+            
             </div>
-            <div role="tabpanel" class="tab-pane fade" id="about">
-            <br>
+            
+
+
+
+            <div role="tabpanel" class="tab-pane fade" id="kontakt">
+
+
+             <br>
+             <br>
             <p><strong> &nbsp Ansvarig mäklare: </strong>${this.firstName} ${this.lastName} </p>
             <p><strong> &nbsp Mejl: </strong>${this.email} </p>
             <p><strong> &nbsp Telefon: </strong>${this.phoneNr} </p>
             <img src ="/${this.realtorPic}" style="width: 18rem;" >
             
             
+            
             </div>
+            
+
+
         	</div>
+        </div>
           
          
         
@@ -86,12 +109,7 @@ class Objekt extends Base {
 
 
         
-        
-        <h5>
-           <br>
-            <a href="/databas/Addres/${this.Residence}">${this.Area}</a>
-            </h5>
-        <h1>${this.title}</h1>
+     
        
             
           
