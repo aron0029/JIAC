aff393ddd8d1f154279e645599951071605ecca2
LadyQuinn <kiaa887@hotmail.com>
2020-03-27 13:04:13 +0100

    edited text

--- www/js/Kontaktaoss.js
+++ www/js/Kontaktaoss.js
@@ -38,12 +38,14 @@ class Kontaktaoss extends Base {
           <br>
         
           <p></p>
-        Behöver du hjälp med att köpa eller sälja din bostad? Hos oss får du hjälp av Stockholms bästa mäklare!
+        Behöver du hjälp med att köpa eller sälja din bostad? Hos oss får du hjälp av Stockholms bästa mäklare!<br>
+        Besök oss gärna på vårt huvudkontor eller mejla in till oss dina frågor eller funderingar!
         <br>
-        <br>
-        Kontaktinfo:
+        <br><br>
+<br>
+<br><br>
+        Huvudkontor:
           <br>
-
 Dhyr & Rumson Sverige AB
 <br>
 Box 4678
@@ -56,7 +58,6 @@ Org.nr: 556444-4593
           
           <br>
         <br>
-          
         </h6>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5018.099597491717!2d18.085618635540456!3d59.33227023793569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d5108d06f43%3A0x1919bf925b4f7e2f!2sStrandv%C3%A4gen%2C%20Stockholm!5e0!3m2!1ssv!2sse!4v1585068750631!5m2!1ssv!2sse" width="500" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
