78d5957188702bb34c16b00c5dfa8f8726161abf
aron0029 <aronsellstedt@gmail.com>
2020-03-27 10:47:39 +0100

    this is objekt fix

--- www/js/Objekt.js
+++ www/js/Objekt.js
@@ -29,29 +29,74 @@ class Objekt extends Base {
         <div class="row" route="/objekt/:id" page-title="Objekt">
     <div page-Area="${this.Area}">
         <div class="col-12">
-         
+
+
+
+       <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
+        	<br />
+
+        	<ul class="nav nav-tabs nav-justified">
+        		<li class="nav-item">
+        			<a href="#home" class="nav-link active" role="tab" data-toggle="tab">Info</a>
+        		</li>
+        		<li class="nav-item">
+        			<a href="#profile" class="nav-link" role="tab" data-toggle="tab">Bilder</a>
+        		</li>
+        		<li class="nav-item">
+        			<a href="#about" class="nav-link" role="tab" data-toggle="tab">Kontakt</a>
+        		</li>
+        	</ul>
+          <div class="tab-content">
+
+
+            <div role="tabpanel" class="tab-pane fade in active" id="home">
+            <br>
+              <p><strong> &nbsp Pris: </strong>${this.price}</p>
+              <p><strong> &nbsp Område: </strong>${this.area}</p>
+              <p><strong> &nbsp Kvm: </strong>${this.Kvm}</p>
+              <p><strong> &nbsp Rum: </strong>${this.rooms}</p>
+              <p><strong> &nbsp Avgift: </strong>${this.rent}</p>
+              <p><strong> &nbsp Adress: </strong>${this.streetName} ${this.streetNumber}</p>
+              <p><strong> &nbsp Församling: </strong>${this.community}</p>
+              <p><strong> &nbsp Förening: </strong>${this.association}</p>
+              
+            
+            </div>
+            
+            <div role="tabpanel" class="tab-pane fade" id="profile">
+            <img  class="card-img" src= "${this.pics}">
+            <img src ="${this.pic}" style="width: 18rem;" >
+            
+            
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="about">
+            <br>
+            <p><strong> &nbsp Ansvarig mäklare: </strong>${this.firstName} ${this.lastName} </p>
+            <p><strong> &nbsp Mejl: </strong>${this.email} </p>
+            <p><strong> &nbsp Telefon: </strong>${this.phoneNr} </p>
+            <img src ="/${this.realtorPic}" style="width: 18rem;" >
+            
+            
+            </div>
+        	</div>
           
-                
+         
+        
               
 
 
         
         
         <h5>
-            Till salu: <br>
+           <br>
             <a href="/databas/Addres/${this.Residence}">${this.Area}</a>
             </h5>
         <h1>${this.title}</h1>
        
             
-              <script>
-               this.pics = pics.split(',');
-            console.log(this.pics);
-            for (x of pics) {
-              document.write(x + "<br >");
-                 }
+          
              
-            var pics = <img  class="card-img" src= "${this.pics}">
+          
             
              
         
@@ -69,29 +114,10 @@ class Objekt extends Base {
 
 
 
-            <div role="tabpanel" class="tab-pane fade" id="about">
+           
             <br>
             
-            <div class="col-lg-3 mb-3 mb-lg-0"> 
-            <span class=""> ${this.price}Kr</span>
-              <br>
-            Rum: ${this.rooms}</span>
-                <div class="col-lg-3 mb-3 mb-lg-0">
-              <span class="">${this.Kvm} Kvadratmeter:</span>
-             <span class="">Avgift:${this.rent}</span>
-            </div>
-            <div class="col-lg-3 mb-3 mb-lg-0">
-              <span class="b">Området: ${this.area}</span>
-              <br> Förening: <span class="c">${this.community}</span> 
-                  
-              <br> <br><span class=""> Mäklare <Strong>${this.firstName}
-              <span class=""> ${this.lastName} </strong>
-              <p class="card-text">Kontakta mig nedan.</p>
-             
-                <a href="mailto:${this.email}" class="card-link">${this.email}</a>
-      
-              
-                 <img class="card-img" src="/${this.realtorPic}"> 
+            
              
             </div>
           
