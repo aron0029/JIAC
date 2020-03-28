1c4760f921544b1cbc8fa6f3047112f4a1a7bc73
FireRay <jonashoglund282@gmail.com>
2020-03-28 08:46:34 +0100

    Small changes for better responsivness in Kontaktaoss class

--- www/js/Kontaktaoss.js
+++ www/js/Kontaktaoss.js
@@ -31,7 +31,8 @@ class Kontaktaoss extends Base {
   render() {
     return /*html*/`
       <div class="row" route="/kontaktaOss" page-title="kontakt">
-        <div class="col-6">
+
+        <div class="col-sm-6">
         <br>
           
           <h1 class="display-4">Kontakta oss</h1>
@@ -59,13 +60,13 @@ Org.nr: 556444-4593
           <br>
         <br>
         </h6>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5018.099597491717!2d18.085618635540456!3d59.33227023793569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d5108d06f43%3A0x1919bf925b4f7e2f!2sStrandv%C3%A4gen%2C%20Stockholm!5e0!3m2!1ssv!2sse!4v1585068750631!5m2!1ssv!2sse" width="500" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+          <iframe class="col-sm-6" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5018.099597491717!2d18.085618635540456!3d59.33227023793569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d5108d06f43%3A0x1919bf925b4f7e2f!2sStrandv%C3%A4gen%2C%20Stockholm!5e0!3m2!1ssv!2sse!4v1585068750631!5m2!1ssv!2sse" width="500" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
 </div>
 
 
 
-<div class="col-6; p-3 mb-2 bg-light">
+<div class="center; col-sm-6; p-3 mb-2; pr-2 mr-2 ; bg-light">
 
 
 ${this.formSent ? /*html*/`
